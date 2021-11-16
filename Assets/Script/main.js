7f9876b5341a241d10f03f90ec55683d31a2b597
Japagod <80219032+Japagod@users.noreply.github.com>
2021-11-16 19:06:21 -0300

    Add files via upload

--- Assets/Script/main.js
+++ Assets/Script/main.js
@@ -1,6 +1,12 @@
 var dataatual = new Date()
 var diaatual = dataatual.getDate()
 var main = document.getElementById("main")
+var horario = 0
+var anim_intro = document.getElementById("anim-intro")
+var anim_intro_img = document.getElementById("anim-intro-img")
+var [minuto, dia, hora, mes] = "0"
+var key = JSON.parse(window.localStorage.getItem('key'))
+var datalength = JSON.parse(window.localStorage.getItem('datalength'))
 const api = "https://discord-moodle-bot.herokuapp.com/api"
 async function getISS() {
     const resposta = await fetch(api)
@@ -18,76 +24,95 @@ async function getISS() {
         window.localStorage.setItem('descrição' + i, JSON.stringify(descrição))
         window.localStorage.setItem('curso' + i, JSON.stringify(curso))
     }
-    alert("Por favor, reinicie a página")
+    anim_intro.style.animation = "anim-intro-end 1s forwards"
+    console.log("CHAMANDO DADOS")
+    pega_dados()
 }
-var key = JSON.parse(window.localStorage.getItem('key'))
-var datalength = JSON.parse(window.localStorage.getItem('datalength'))
-
-if (diaatual - key > 1) {
-    localStorage.clear
-    getISS()
-} else {
-    for (var i = 0; i < datalength; i++) {
-        var nome = JSON.parse(window.localStorage.getItem('nome' + i))
-        var date = JSON.parse(window.localStorage.getItem('date' + i))
-        var horario = new Date(date)
-        var [minuto, dia, hora, mes] = [horario.getUTCMinutes(), horario.getDate(), horario.getHours(), horario.getUTCMonth()]
-        var descrição = JSON.parse(window.localStorage.getItem('descrição' + i))
-        var curso = JSON.parse(window.localStorage.getItem('curso' + i))
-
-
-        // Construtor
-
-
-
-        // Titulo
-
-
-        var art = document.createElement("article")
-        art.classList.add("article")
-        var txt = document.createTextNode(curso)
-        var p = document.createElement("p")
-        var tit = document.createElement("h1")
-        tit.appendChild(txt)
-        art.appendChild(tit)
-        main.appendChild(art)
-
-
-        txt = document.createTextNode(nome)
-        p.appendChild(txt)
-        art.appendChild(p)
-        main.appendChild(art)
-
-        var div = document.createElement("div")
-        div.classList.add("conteudo")
-        div.innerHTML = descrição
-        art.appendChild(div)
-        main.appendChild(art)
-
-
-
-
-
-        // DATA DE ENTREGA
-
-
-
-
-        if (2 > minuto.toString().length) {
-            var dataentrega = document.createTextNode("A data de entrega é: " + dia + " do " + mes + " as " + hora + ":0" + minuto)
-        } else {
-            var dataentrega = document.createTextNode("A data de entrega é: " + dia + " do " + mes + " as " + hora + ":" + minuto)
+function pega_dados() {
+        datalength = JSON.parse(window.localStorage.getItem('datalength'))
+        key = JSON.parse(window.localStorage.getItem('key'))
+    console.log("chamou1")
+    if (diaatual - key > 1) {
+        console.log("chamou2")
+        for(var i = 0;i<datalength;){
+            localStorage.removeItem("datalength","key","nome"+i,"date"+i,"descrição"+i,"curso"+i)
         }
-        art.appendChild(dataentrega)
-        main.appendChild(art)
-
-        user_time_span = JSON.parse(window.localStorage.getItem('user_time_span'))
-        if (dia - diaatual <= user_time_span) {
-            art.style.border = "red  solid  3px"
-            art.style.boxShadow = "0px 0px 50px 5px red"
-        } else {
-            art.style.border = "green  solid  3px"
-            art.style.boxShadow = "0px 0px 50px 5px green"
+        console.log("CHAMANDO GETISS")
+        anim_intro.style.display = "flex"
+        anim_intro_img.style.animation = "anim-intro 3s infinite"
+        getISS()
+    } else if (diaatual - key < 1) {
+        for (var i = 0; i < datalength; i++) {
+            console.log("chamou4")
+            nome = JSON.parse(window.localStorage.getItem('nome' + i))
+            date = JSON.parse(window.localStorage.getItem('date' + i))
+            horario = new Date(date)
+            minuto = horario.getMinutes()
+            dia = horario.getDate()
+            hora = horario.getHours()
+            mes = horario.getMonth()
+            descrição = JSON.parse(window.localStorage.getItem('descrição' + i))
+            curso = JSON.parse(window.localStorage.getItem('curso' + i))
+            console.log("CHAMANDO CONSTRUTOR")
+            construtor()
         }
     }
 }
+
+function construtor() {
+
+    // Titulo
+
+
+    var art = document.createElement("article")
+    art.classList.add("article")
+    var txt = document.createTextNode(curso)
+    var p = document.createElement("p")
+    var tit = document.createElement("h1")
+    tit.appendChild(txt)
+    art.appendChild(tit)
+    main.appendChild(art)
+
+
+    txt = document.createTextNode(nome)
+    p.appendChild(txt)
+    art.appendChild(p)
+    main.appendChild(art)
+
+    var div = document.createElement("div")
+    div.classList.add("conteudo")
+    div.innerHTML = descrição
+    art.appendChild(div)
+    main.appendChild(art)
+
+
+
+
+
+    // DATA DE ENTREGA
+
+
+
+
+    if (2 > minuto.toString().length) {
+        var dataentrega = document.createTextNode("A data de entrega é: " + dia + " do " + mes + " às " + hora + ":0" + minuto)
+    } else {
+        var dataentrega = document.createTextNode("A data de entrega é: " + dia + " do " + mes + " às " + hora + ":" + minuto)
+    }
+    div = document.createElement("div")
+    div.classList.add("bah")
+    div.appendChild(dataentrega)
+    art.appendChild(div)
+    main.appendChild(art)
+
+    user_time_span = JSON.parse(window.localStorage.getItem('user_time_span'))
+    if (dia - diaatual <= user_time_span) {
+        art.style.border = "red  solid  3px"
+        art.style.boxShadow = "0px 0px 50px 5px red"
+    } else {
+        art.style.border = "green  solid  3px"
+        art.style.boxShadow = "0px 0px 50px 5px green"
+    }
+}
+console.log("CHAMANDO DADOS")
+pega_dados()
--- Assets/Script/user__options.js
+++ Assets/Script/user__options.js
@@ -1,35 +1,56 @@
 var nav = document.getElementById("nav")
 var onoff = false
-var dark = false
 document.getElementById("config").addEventListener("click", e => {
     if (onoff == false) {
-        nav.style.display = "flex"
+        nav.style.animation = "anim-config 1s forwards"
+        main.style.animation = "anim-config-blur 1s forwards"
         onoff = true
     } else {
-        nav.style.display = "none"
+        main.style.animation = "anim-config-blur-out 1s forwards"
+        nav.style.animation = "anim-config-out 1s forwards"
         onoff = false
     }
 })
+dark = JSON.parse(window.localStorage.getItem('dark'))
+if (dark == true) {
+    document.getElementById("main").style.backgroundColor = "#121212"
+    document.getElementById("light_dark").style.filter = "invert(96%) sepia(40%) saturate(0%) hue-rotate(292deg) brightness(113%) contrast(101%)"
+    document.getElementById("nav").style.backgroundColor = "#121212"
+    document.getElementById("nav_label").style.color = "#fff"
+    
 
+    
+} else {
+    document.getElementById("main").style.backgroundColor = "#fff"
+    document.getElementById("light_dark").style.filter = "invert(0%) sepia(3%) saturate(23%) hue-rotate(84deg) brightness(96%) contrast(100%)"
+    document.getElementById("nav").style.backgroundColor = "#fff"
+    document.getElementById("nav_label").style.color = "#121212"
+
+   
+}
 document.getElementById("light_dark").addEventListener("click", e => {
+    dark = JSON.parse(window.localStorage.getItem('dark'))
     if (dark == false) {
         document.getElementById("main").style.backgroundColor = "#121212"
         document.getElementById("light_dark").style.filter = "invert(96%) sepia(40%) saturate(0%) hue-rotate(292deg) brightness(113%) contrast(101%)"
         document.getElementById("nav").style.backgroundColor = "#121212"
         document.getElementById("nav_label").style.color = "#fff"
+        
 
-
-
-        dark =true
+        dark=true
+        console.log(dark)
+        window.localStorage.setItem('dark', JSON.stringify(dark))
     } else {
         document.getElementById("main").style.backgroundColor = "#fff"
         document.getElementById("light_dark").style.filter = "invert(0%) sepia(3%) saturate(23%) hue-rotate(84deg) brightness(96%) contrast(100%)"
         document.getElementById("nav").style.backgroundColor = "#fff"
         document.getElementById("nav_label").style.color = "#121212"
 
-
         dark = false
+        console.log(dark)
+        window.localStorage.setItem('dark', JSON.stringify(dark))
     }
+
 })
 
 function userspan() {
